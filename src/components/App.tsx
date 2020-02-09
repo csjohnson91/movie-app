9d56ca01c73471c163c98582bc359c011ba89740
Chris Johnson <csjohnson91@gmail.com>
2020-02-09 11:29:37 +1000

    Sorted out flex boxing of MovieList

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import '../styles/App.css';
 import Header from "./Header";
-import { Container, Row } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 
 function App() {
   return (
     <div className='app'>
-      <Container>
+      <Container className='verticallyPadded'>
         <Row>
-          <Header/>
+          <Col sm="12" md={{ size: 6, offset: 3 }} className='verticallyPadded'>
+            <Header/>
+          </Col>
         </Row>
         <Row>
-          <SearchBar/>
+          <Col sm="12" md={{ size: 6, offset: 3 }} className='verticallyPadded'>
+            <SearchBar/>
+          </Col>
         </Row>
-        <Row>
-          <MovieList/>
-        </Row>
+        <MovieList/>
       </Container>
     </div>
   );
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,18 +1,23 @@
 import * as React from "react";
 import MovieListItem from "./MovieListItem";
 import movieInfo from './../movieInfo.json'
-import { Switch, useRouteMatch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import MovieDetail from "./MovieDetail";
+import { Col, Row } from "reactstrap";
+import './../styles/MovieList.css'
+
 
 const MovieList = () => {
   return (
     <Switch>
       <Route exact path='/'>
-        <div className="movies">
-          {movieInfo.map(movie =>
+        <Row className='movieListRow'>
+        {movieInfo.map(movie =>
+          <Col className='movieListCol'>
             <MovieListItem title={movie.name} posterPath={movie.image} id={movie.id}/>
-          )}
-        </div>
+          </Col>
+        )}
+        </Row>
       </Route>
       <Route path={'/:movieId'}>
         <MovieDetail />
