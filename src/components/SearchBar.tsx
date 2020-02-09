ca687fc9abd4ec32931e006fba787ea549fbbc8a
Chris Johnson <csjohnson91@gmail.com>
2020-02-09 11:30:02 +1000

    Styled SearchBar.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,25 @@
 import * as React from "react";
+import { Button, Input, InputGroup, InputGroupAddon } from "reactstrap";
+import { FaSearch } from "react-icons/fa";
+import './../styles/SearchBar.css'
 
 const SearchBar = () => {
   return (
-    <form>
-      <input
-        placeholder="Search for..."
-        // TODO: make functional AND pretty
-        //value={this.state.query}
-        //onChange={this.handleInputChange}
-      />
-    </form>
+    <InputGroup>
+          <Input
+            className='searchBarInput'
+            placeholder="Search"
+            type='search'
+            name='search'
+            id='searchBar'
+            // TODO: make functional AND pretty
+            //value={this.state.query}
+            //onChange={this.handleInputChange}
+          />
+          <InputGroupAddon addonType='append'>
+            <Button ><FaSearch className='textGreen'/></Button>
+          </InputGroupAddon>
+    </InputGroup>
   )
 };
 
