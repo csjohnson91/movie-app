6c3c0810b8e6d9c039a5ae9e3e4991ab3db0018c
Chris Johnson <csjohnson91@gmail.com>
2020-02-11 22:35:54 +1000

    Added back button to DetailBackdrop.

--- src/components/DetailBackdrop.tsx
+++ src/components/DetailBackdrop.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
+import { FaArrowLeft } from "react-icons/fa";
+import '../styles/DetailBackdrop.css'
+import { Link } from "react-router-dom";
 
 type DetailBackdrop = {
   height: number
   src: string,
   alt: string
 }
-// TODO: add back button
+
 const DetailBackdrop = ({ height, src, alt }: DetailBackdrop) => {
+
+  const imageStyle: CSSProperties = {
+    opacity: '50%',
+    height: height,
+    overflow: 'hidden'
+  };
+
   return (
-    <div style={{ opacity: '50%', height: height, overflow: 'hidden' }}>
-      <img src={src} alt={alt}/>
+    <div className='backdrop-container'>
+      <div style={imageStyle}>
+        <img src={src} alt={alt}/>
+      </div>
+      <Link to='/'><div className='back-button'><FaArrowLeft /></div></Link>
     </div>
   )
 };
