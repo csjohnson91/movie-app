c23ae7586387f9290f884c71a320356f7d8cdba9
Chris Johnson <csjohnson91@gmail.com>
2020-02-12 17:12:47 +1000

    Increased popular movies rendered from 20 to 40.

--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -6,18 +6,30 @@ import MovieList from './MovieList';
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
 
+/* Note that this is a bit hacky in the way that I'm composing two paginated responses to show the 40 most popular
+ movies. In future this should be changed to some sort of infinite paged scroll solution. */
+
+const composeResponses = (response1: Data, response2: Data) => {
+  if (!response1 || !response2) {
+    return {}
+  }
+  return { ...response1.data, results: [ ...response1.data.results, ...response2.data.results ] }
+};
+
 const PopularMovies = () => {
   const [popularMovies, setPopularMovies] = useState<Data>(null);
-  const url = getPopularMovieUrl(1);
+  const page1Url = getPopularMovieUrl(1);
+  const page2Url = getPopularMovieUrl(2);
 
   useEffect(() => {
       const fetchData = async () => {
-        const response = await Axios(url);
-        setPopularMovies(response.data);
+        const response1 = await Axios(page1Url);
+        const response2 = await Axios(page2Url);
+        setPopularMovies(composeResponses(response1, response2));
       };
       fetchData();
 
-    }, [url]
+    }, [page1Url, page2Url]
   );
 
   if (!popularMovies) {
@@ -26,7 +38,8 @@ const PopularMovies = () => {
     return <div>Whoops! Something is very wrong! Go <Link to='/'>home</Link> and try again</div>
   } else {
     const movies: Array<{ [key: string]: any }> = popularMovies.results;
-    // TODO: in future the real number could be passed in here, but it's currently only used to determine if there are ANY results anyway
+    /* in future the real number could be passed in here, but it's currently only used to determine
+     if there are ANY results anyway */
     return <MovieList numberOfResults={1} listTitle='Popular Movies' movies={movies}/>
   }
 };
