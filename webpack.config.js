acc3f36c52542b3153450d15ee0384c8172b8cf9
Chris Johnson <csjohnson91@gmail.com>
2020-02-12 13:09:32 +1000

    Added IE/Edge compatibility.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: 'ts-loader'
+        loader: 'babel-loader'
       },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
