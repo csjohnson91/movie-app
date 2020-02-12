73cf947ff1289427524ee5e88d684cd01baceefe
Chris Johnson <csjohnson91@gmail.com>
2020-02-12 15:43:11 +1000

    Made header image link to root.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import logo from '../images/tmdb-logo.png'
 import '../styles/Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <img src={logo} alt='Logo' className='header-logo'/>
+  return <Link to='/'><img src={logo} alt='Logo' className='header-logo'/></Link>
 };
 
 export default Header;
